85f28766fc027bf0e1c4c975d127ab7d355f5c86
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-10-21 18:59:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 nose_x = 02;
 nose_y = 02;
+eyel = 02;
+eyer = 02;
 
 function preload(){
-    treacleTart = loadImage("nose.png");
+    sausage = loadImage("https://i.postimg.cc/T20T3tq3/gagagaga.png");
+    nose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
 }
 
 function setup(){
@@ -22,6 +25,8 @@ function pumpkinJuice(results){
     if(results.length > 0){
         nose_x = results[0].pose.nose.x;
         nose_y = results[0].pose.nose.y;
+        eyel = results[0].pose.leftEye.x-97;
+        eyer = results[0].pose.leftEye.y-20;
         console.log("x - nose      " + nose_x + "    y - nose    " + nose_y);
         console.log(results);
     }
@@ -34,6 +39,11 @@ function modelloaded(){
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(nose_x, nose_y, 20);
+    image(nose, nose_x - 25, nose_y - 25, 45, 45);
+    image(sausage, eyel, eyer, 150, 67);
 }
 
 function take_picture(){
